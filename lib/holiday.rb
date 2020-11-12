76819d3b5906b8fff9faacf4e2e71d6b30d6c7e6
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-12 13:57:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,10 +62,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-return holiday_hash[:winter].values
-return holiday_hash[:summer].values
-return holiday_hash[:fall].values
-return holiday_hash[:spring].values
+  holiday_hash.each do | season, holiday|
+       puts "#{season.capitalize}:"
+       holiday.each do |holiday, item|
+         puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+       end
+    end
 
 end
 
